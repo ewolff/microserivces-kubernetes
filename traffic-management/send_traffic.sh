1e7f0d120aa09ab5c4cf6380610427a997580645
Chris Doyle <uk.chris.doyle+cdsre@gmail.com>
2024-06-27 20:16:25 +0100

    updated traffic shapping

--- traffic-management/send_traffic.sh
+++ traffic-management/send_traffic.sh
@@ -3,7 +3,7 @@ version=$1
 output=/tmp/$$
 node_port=$(kubectl get service -n consul api-gateway -o=jsonpath='{.spec.ports[0].nodePort}')
 for i in $(seq 1 100); do
-    curl -s "http://localhost:${node_port}/?version=${version}" | grep h1 | tee -a $output
+    curl -s "http://localhost:${node_port}/customer?version=${version}" | grep href | grep -v profile | tee -a $output
 done
 
 echo "################"
